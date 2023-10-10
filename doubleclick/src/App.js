ebecc7120151e397ae6ffbb77ac330973e3a4e70
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-10 12:16:33 +0530

    Double-Click  updated

---
+++ doubleclick/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DoubleClick from './Components/DoubleClick';
+
+function App() {
+  return (
+    <div >
+      <DoubleClick/>
+    </div>
+  );
+}
+
+export default App;
